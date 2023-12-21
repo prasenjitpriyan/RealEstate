41dd0135c669abfdb2003ad1a76b3a1a3587cfb0
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2023-12-21 07:30:41 +0530

    Backend ready

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
-import express from "express"
-import { google, signup, signin } from "../controllers/auth.controller.js"
+import express from 'express';
+import { google, signOut, signin, signup } from '../controllers/auth.controller.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup", signup)
+router.post("/signup", signup);
 router.post("/signin", signin);
 router.post('/google', google);
+router.get('/signout', signOut)
 
-export default router
+export default router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv"
-import userRouter from "./routes/user.route.js";
-import authRouter from './routes/auth.route.js'
-dotenv.config()
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,15 +17,28 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 })
 
-app.use("/api/user", userRouter)
-app.use("/api/auth", authRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
