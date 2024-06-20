a73d5233568ba5b31afbdb43d221bc2844a3a1d9
Prasenjit Das <132381731+prasenjitpriyan@users.noreply.github.com>
2024-06-20 09:00:09 +0530

    Update ListingItem.jsx

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,7 +3,6 @@ import { MdLocationOn } from "react-icons/md";
 import { EmailShareButton, EmailIcon } from "react-share";
 
 export default function ListingItem({ listing }) {
-  console.log(listing);
   return (
     <div className="bg-primary shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
